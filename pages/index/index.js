6ae4b7f94df0598a406d05fbcfddeb344f228bd1
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-08-06 22:49:40 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,33 +3,29 @@
 const app = getApp()
 
 Page({
-  data: {
-   
-  },
-  
-  onLoad: function () {
-   
-  },
+
   openSetting: function () {
     wx.getSetting({
       success(res) {
-        console.log(22,res)
+        console.log(22,res)   
       }
     })
   },
 
-  checkAuthorize: function () {
-    
+  grantAuthorizeLocation: function () { 
     wx.authorize({
       scope: 'scope.userLocation',
       success(res) {
-          console.log(333,res)
-          
+          console.log(33,res)
+          wx.chooseLocation({
+            success: function(res) {
+              console.log(44,res)
+            },
+          })
       },
       fail(err) {
         console.log(44,err)
       }
-      })
-
+    })
   }
 })
